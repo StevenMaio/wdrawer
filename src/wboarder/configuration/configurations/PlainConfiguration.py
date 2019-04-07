34c44014f5e0a8d052bdda0077b56696f8ef7d42
Steven Maio <stevenmaio.321@gmail.com>
2019-04-06 23:45:40 -0400

    Added PlainConfiguration class
    This class isn't finished yet, but it's the rough outline of an
    implementation of the Configuration class

---
+++ src/wboarder/configuration/configurations/PlainConfiguration.py
@@ -0,0 +1,47 @@
+##  @package wboarder.configuration.configurations.PlainConfiguration
+#
+#   Contains the PlainConfiguration class.
+#
+#   \author Steven Maio
+
+from src.wboarder.configuration.Configuration import  Configuration
+from src.wboarder.image_processor.ImageProcessor import ImageProcessor
+from src.wboarder.drawer.Drawer import Drawer
+from src.wboarder.drawer.drawers.PrintDrawer import PrintDrawer
+from src.wboarder.pather.LeftToRightPather import LeftToRightPather
+from src.wboarder.pather.Pather import Pather
+
+
+##  A simple implementation of the Configuration class.
+#
+class PlainConfiguration(Configuration):
+
+    ##  Constructs a new instance of PlainConfiguration
+    #
+    def __init__(self):
+        pass
+
+    ##  Overrided init method. For this implementation, this method is not
+    #   necessary
+    #
+    #   @param args a list of arguments to the constructor
+    def init(args : list) -> None:
+        pass
+
+    ##  Returns a basic ImageProcessor implementation
+    #
+    #   @return an instance of ImageProcessor
+    def initImageProcessor(self) -> ImageProcessor:
+        pass
+
+    ##  Returns an instance of LeftToRightPather
+    #
+    #   @return an instance of LeftToRightPather
+    def initpather(self) -> Pather:
+        return LeftToRightPather()
+
+    ##  Returns an instace of PrintDrawer
+    #
+    #   @return an instance of PrintDrawer
+    def initDrawer(self) -> Drawer:
+        return PrintDrawer()
